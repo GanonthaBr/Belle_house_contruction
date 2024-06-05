563a25f6ed139d2e8ec11691afa4c1d8a2ec8afc
Bruno <bganontha@gmail.com>
2024-06-05 11:33:48 +0100

    chore: Add navbar and topbar blade templates

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container d-flex align-items-center">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="fa fa-bars"></span> Menu
+            </button>
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active"><a href="index.html" class="nav-link">Accueil</a></li>
+                    <li class="nav-item"><a href="about.html" class="nav-link">A propos</a></li>
+                    <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="project.html" class="nav-link">Projets</a></li>
+                    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+                </ul>
+                <a href="#" class="btn-custom" data-toggle="modal" data-target="#exampleModalCenter">Nous Contacter</a>
+            </div>
+        </div>
+    </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,81 +1,8 @@
 @extends('layouts.layout')
 @section('content')
     
-    <div class="py-1 top">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm text-center text-md-left mb-md-0 mb-2 pr-md-4 d-flex topper align-items-center">
-                    <p class="mb-0 w-100">
-                        <span class="fa fa-paper-plane"></span>
-                        <span class="text"><a href="https://preview.colorlib.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="9de4f2e8eff8f0fcf4f1ddf8f0fcf4f1b3fef2f0" style="font-size: 12px">contact@bellehouseniger.com</a></span>
-                    </p>
-                </div>
-                <div class="col-sm justify-content-center d-flex mb-md-0 mb-2">
-                    <div class="social-media">
-                        <p class="mb-0 d-flex">
-                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
-                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
-                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
-                            {{-- <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a> --}}
-
-                        </p>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6 col-lg-7 d-flex topper align-items-center text-lg-right justify-content-end">
-                    <p class="mb-0 register-link"><a href="#" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Nous Contacter</a></p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="pt-4 pb-5">
-        <div class="container">
-            <div class="row d-flex align-items-start align-items-center px-3 px-md-0">
-                <div class="col-md-4 d-flex mb-2 mb-md-0">
-                    <a class="navbar-brand d-flex align-items-center" href="index.html">
-                        <span>
-                            <img src="images/logo.png" alt="Belle House Construction Company" height="65" width="80">
-                        </span>
-                        <span class="ml-2">Belle House <small style="font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif">Batisseur de Confiance</small></span>
-                    </a>
-                </div>
-                <div class="col-md-4 d-flex topper mb-md-0 mb-2 align-items-center">
-                    <div class="icon d-flex justify-content-center align-items-center">
-                        <span class="fa fa-map"></span>
-                    </div>
-                    <div class="pr-md-4 pl-md-3 pl-3 text">
-                        <p class="con"><span>Appellez au</span> <span>+227 456 78910</span></p>
-                        <p class="con">Appel 24/7 pour vos besoins</p>
-                    </div>
-                </div>
-                <div class="col-md-4 d-flex topper mb-md-0 align-items-center">
-                    <div class="icon d-flex justify-content-center align-items-center"><span class="fa fa-paper-plane"></span>
-                    </div>
-                    <div class="text pl-3 pl-md-3">
-                        <p class="hr"><span>Localisation</span></p>
-                        <p class="con">Niamey Quartier Bobiel,Niger</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
-        <div class="container d-flex align-items-center">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="fa fa-bars"></span> Menu
-            </button>
-            <div class="collapse navbar-collapse" id="ftco-nav">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"><a href="index.html" class="nav-link">Accueil</a></li>
-                    <li class="nav-item"><a href="about.html" class="nav-link">A propos</a></li>
-                    <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-                    <li class="nav-item"><a href="project.html" class="nav-link">Projets</a></li>
-                    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-                </ul>
-                <a href="#" class="btn-custom" data-toggle="modal" data-target="#exampleModalCenter">Nous Contacter</a>
-            </div>
-        </div>
-    </nav>
+    @include('includes.topbar')
+    @include('includes.navbar')
 
     <section class="hero-wrap js-fullheight" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
         <div class="overlay"></div>
