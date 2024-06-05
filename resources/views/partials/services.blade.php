cb9ed8ab3df12ed72022b49e4fcf9255378f6582
Bruno <bganontha@gmail.com>
2024-06-05 11:58:55 +0100

    chore: Update services.blade.php with new service sections

--- resources/views/partials/services.blade.php
+++ resources/views/partials/services.blade.php
@@ -1,30 +1,42 @@
-<section class="ftco-section ftco-no-pt ftco-no-pb ftco-services-2">
+ <section class="ftco-section bg-half-light">
         <div class="container">
-            <div class="row no-gutters d-flex">
-                <div class="col-lg-4 d-flex align-self-stretch ftco-animate">
-                    <div class="media block-6 services d-flex">
-                        <div class="icon justify-content-center align-items-center d-flex"><span class="flaticon-engineer-1"></span></div>
-                        <div class="media-body pl-4">
-                            <h3 class="heading mb-3">Quality Construction</h3>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <div class="row justify-content-center mb-5 pb-2">
+                <div class="col-md-8 text-center heading-section ftco-animate">
+                    <span class="subheading">Our Services</span>
+                    <h2 class="mb-4">We Offer Services</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="services-wrap ftco-animate">
+                        <div class="img" style="background-image: url(images/services-1.jpg);"></div>
+                        <div class="text">
+                            <div class="icon"><span class="flaticon-architect"></span></div>
+                            <h2>Architecture</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                            <a href="#" class="btn-custom">Read more</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 d-flex align-self-stretch ftco-animate">
-                    <div class="media block-6 services services-2 d-flex">
-                        <div class="icon justify-content-center align-items-center d-flex"><span class="flaticon-worker-1"></span></div>
-                        <div class="media-body pl-4">
-                            <h3 class="heading mb-3">Professional Liability</h3>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                <div class="col-md-4">
+                    <div class="services-wrap ftco-animate">
+                        <div class="img" style="background-image: url(images/services-2.jpg);"></div>
+                        <div class="text">
+                            <div class="icon"><span class="flaticon-worker"></span></div>
+                            <h2>Renovation</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                            <a href="#" class="btn-custom">Read more</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 d-flex align-self-stretch ftco-animate">
-                    <div class="media block-6 services d-flex">
-                        <div class="icon justify-content-center align-items-center d-flex"><span class="flaticon-engineer"></span></div>
-                        <div class="media-body pl-4">
-                            <h3 class="heading mb-3">Dedicated To Our Clients</h3>
-                            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                <div class="col-md-4">
+                    <div class="services-wrap ftco-animate">
+                        <div class="img" style="background-image: url(images/services-3.jpg);"></div>
+                        <div class="text">
+                            <div class="icon"><span class="flaticon-hammer"></span></div>
+                            <h2>Construction</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                            <a href="#" class="btn-custom">Read more</a>
                         </div>
                     </div>
                 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     @include('includes.topbar')
     @include('includes.navbar')
     @include('partials.hero')
-    @include('partials.services')
+    @include('partials.missions')
     @include('partials.about')
     <section class="ftco-intro">
         <div class="container">
@@ -20,51 +20,7 @@
             </div>
         </div>
     </section>
-    <section class="ftco-section bg-half-light">
-        <div class="container">
-            <div class="row justify-content-center mb-5 pb-2">
-                <div class="col-md-8 text-center heading-section ftco-animate">
-                    <span class="subheading">Our Services</span>
-                    <h2 class="mb-4">We Offer Services</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="services-wrap ftco-animate">
-                        <div class="img" style="background-image: url(images/services-1.jpg);"></div>
-                        <div class="text">
-                            <div class="icon"><span class="flaticon-architect"></span></div>
-                            <h2>Architecture</h2>
-                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                            <a href="#" class="btn-custom">Read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="services-wrap ftco-animate">
-                        <div class="img" style="background-image: url(images/services-2.jpg);"></div>
-                        <div class="text">
-                            <div class="icon"><span class="flaticon-worker"></span></div>
-                            <h2>Renovation</h2>
-                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                            <a href="#" class="btn-custom">Read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="services-wrap ftco-animate">
-                        <div class="img" style="background-image: url(images/services-3.jpg);"></div>
-                        <div class="text">
-                            <div class="icon"><span class="flaticon-hammer"></span></div>
-                            <h2>Construction</h2>
-                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                            <a href="#" class="btn-custom">Read more</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('partials.services')
     <section class="ftco-section ftco-no-pt ftco-no-pb ftco-counter">
         <div class="img image-overlay" style="background-image: url(images/about-3.jpg);"></div>
         <div class="container">
@@ -103,93 +59,7 @@
         </div>
     </section>
     @include('partials.projects')
-    <section class="ftco-section ftco-no-pt ftco-no-pb testimony-section img">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row ftco-animate justify-content-center">
-                <div class="col-md-6 p-4 pl-md-0 py-md-5 pr-md-5 aside-stretch d-flex align-items-center">
-                    <div class="heading-section heading-section-white">
-                        <span class="subheading" style="color:#fff;">Read Testimonials</span>
-                        <h2 class="mb-4" style="font-size: 50px;">It's always a joy to hear that the work we do has positively reviews</h2>
-                    </div>
-                </div>
-                <div class="col-md-6 pl-md-5 py-4 py-md-5 aside-stretch-right">
-                    <div class="carousel-testimony owl-carousel ftco-owl">
-                        <div class="item">
-                            <div class="testimony-wrap py-4 pb-5 d-flex justify-content-between align-items-end">
-                                <div class="user-img" style="background-image: url(images/person_1.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="fa fa-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text">
-                                    <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean</p>
-                                    <p class="name">Jeff Freshman</p>
-                                    <span class="position">Guests</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimony-wrap py-4 pb-5 d-flex justify-content-between align-items-end">
-                                <div class="user-img" style="background-image: url(images/person_2.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="fa fa-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text">
-                                    <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean</p>
-                                    <p class="name">Jeff Freshman</p>
-                                    <span class="position">Guests</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimony-wrap py-4 pb-5 d-flex justify-content-between align-items-end">
-                                <div class="user-img" style="background-image: url(images/person_3.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="fa fa-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text">
-                                    <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean</p>
-                                    <p class="name">Jeff Freshman</p>
-                                    <span class="position">Guests</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimony-wrap py-4 pb-5 d-flex justify-content-between align-items-end">
-                                <div class="user-img" style="background-image: url(images/person_1.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="fa fa-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text">
-                                    <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean</p>
-                                    <p class="name">Jeff Freshman</p>
-                                    <span class="position">Guests</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimony-wrap py-4 pb-5 d-flex justify-content-between align-items-end">
-                                <div class="user-img" style="background-image: url(images/person_3.jpg)">
-                                    <span class="quote d-flex align-items-center justify-content-center">
-                                        <i class="fa fa-quote-left"></i>
-                                    </span>
-                                </div>
-                                <div class="text">
-                                    <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean</p>
-                                    <p class="name">Jeff Freshman</p>
-                                    <span class="position">Guests</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('partials.testimonials')
     @include('partials.blogs')
    @include('partials.footer')
    @include('includes.modal')
