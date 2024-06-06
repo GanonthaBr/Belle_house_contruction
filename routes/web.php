c31ca7d8d62aa99055205a11743ea3277ee635f9
Bruno <bganontha@gmail.com>
2024-06-06 11:54:31 +0100

    created controllers and routes for the nav bar menu items

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BlogsController;
+use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\ServicesController;
 
 
 //Home 
@@ -11,3 +15,16 @@ Route::get('/', [HomeController::class, 'index']);
 
 //About
 Route::get('/about', [AboutController::class, 'index'])->name('about');
+
+//Contact
+Route::get('/contacts', [ContactsController::class, 'index'])->name('contacts');
+
+
+//services
+Route::get('/services', [ServicesController::class, 'index'])->name('services');
+
+//blogs
+Route::get('/blogs', [BlogsController::class, 'index'])->name('blogs');
+
+//projects
+Route::get('/projects', [ProjectsController::class, 'index'])->name('projects');
