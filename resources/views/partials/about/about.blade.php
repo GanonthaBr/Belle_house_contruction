ce89dbd1d0749408771a60f0585072cd274b1740
Bruno <bganontha@gmail.com>
2024-06-06 14:28:33 +0100

    added the content for about section on Main page

--- resources/views/partials/about/about.blade.php
+++ resources/views/partials/about/about.blade.php
@@ -9,18 +9,24 @@
                 <div class="col-md-6 py-5 pl-md-5">
                     <div class="row justify-content-center mb-4 pt-md-4">
                         <div class="col-md-12 heading-section ftco-animate">
-                            <span class="subheading">Welcome to Wilcon</span>
-                            <h2 class="mb-4">Wilcon A Construction Company</h2>
+                            <span class="subheading">BIENVENUE CHEZ BELLE HOUSE</span>
+                            <h2 class="mb-4">L’ART DE BATIR AUTREMENT</h2>
                             <div class="d-flex about">
                                 <div class="icon"><span class="flaticon-hammer"></span></div>
-                                <h3>We're in this business since 1975 and We provide the best insdustrial services</h3>
+                                <h3>Depuis bientôt dix ans, notre entreprise se distingue comme une référence incontournable dans le domaine de l'architecture et de la construction moderne. </h3>
                             </div>
-                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                            <p>
+                                Forte de son expertise et de son innovation, notre entreprise s’engage à transformer vos rêves en réalité, en créant des espaces qui allient fonctionnalité, design et durabilité.
+                            </p>
+                            <p>
+                               "Découvrez comment nous collaborons main dans la main avec nos clients pour atteindre leurs objectifs. Regardez la vidéo maintenant pour en savoir plus !"
+                            </p>
+
                             <div class="d-flex video-image align-items-center mt-md-4">
                                 <a href="#" class="video img d-flex align-items-center justify-content-center" style="background-image: url(images/banner-1.jpg);">
                                     <span class="fa fa-play-circle"></span>
                                 </a>
-                                <h4 class="ml-4">This is how we work on our clients, Watch video</h4>
+                                <h4 class="ml-4">Regardez la vidéo</h4>
                             </div>
                         </div>
                     </div>
