62f3899d1534d73b789fc585b2652470d4ded963
Bruno <bganontha@gmail.com>
2024-06-06 13:03:11 +0100

    ajouter le contenu pour la section Service on the Main page

--- resources/views/partials/services/services.blade.php
+++ resources/views/partials/services/services.blade.php
@@ -2,41 +2,41 @@
         <div class="container">
             <div class="row justify-content-center mb-5 pb-2">
                 <div class="col-md-8 text-center heading-section ftco-animate">
-                    <span class="subheading">Our Services</span>
-                    <h2 class="mb-4">We Offer Services</h2>
+                    <span class="subheading">Nos Services</span>
+                    <h2 class="mb-4">Nous offrons des services</h2>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-4">
                     <div class="services-wrap ftco-animate">
-                        <div class="img" style="background-image: url(images/services-1.jpg);"></div>
+                        <div class="img" style="background-image: url(images/services_Architecture.png);"></div>
                         <div class="text">
                             <div class="icon"><span class="flaticon-architect"></span></div>
                             <h2>Architecture</h2>
-                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                            <a href="#" class="btn-custom">Read more</a>
+                            <p>Notre service d'architecture repose sur une approche innovante et créative pour concevoir des espaces qui allient fonctionnalité, esthétique et durabilité. </p>
+                            <a href="#" class="btn-custom">Voir Plus+</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="services-wrap ftco-animate">
-                        <div class="img" style="background-image: url(images/services-2.jpg);"></div>
+                        <div class="img" style="background-image: url(images/services_Renovation.png);"></div>
                         <div class="text">
                             <div class="icon"><span class="flaticon-worker"></span></div>
                             <h2>Renovation</h2>
-                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                            <a href="#" class="btn-custom">Read more</a>
+                            <p>Transformez vos espaces existants en chefs-d'œuvre modernes avec notre service de rénovation. </p>
+                            <a href="#" class="btn-custom">Voir Plus+</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="services-wrap ftco-animate">
-                        <div class="img" style="background-image: url(images/services-3.jpg);"></div>
+                        <div class="img" style="background-image: url(images/services_Construction.png);"></div>
                         <div class="text">
                             <div class="icon"><span class="flaticon-hammer"></span></div>
                             <h2>Construction</h2>
-                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                            <a href="#" class="btn-custom">Read more</a>
+                            <p>Du concept à la réalisation, notre service de construction assure une exécution impeccable de vos projets.</p>
+                            <a href="#" class="btn-custom">Voir Plus+</a>
                         </div>
                     </div>
                 </div>
