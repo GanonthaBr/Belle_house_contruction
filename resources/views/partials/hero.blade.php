b1c6a9235874871ccfbc36c2b004ea54e76cc4ee
Bruno <bganontha@gmail.com>
2024-06-06 10:03:40 +0100

    work on hero section

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,5 +1,5 @@
 
-    <section class="hero-wrap js-fullheight" style="background-image: url('images/banner-1.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap js-fullheight" style="background-image: url('images/banner-3.png');" data-stellar-background-ratio="0.5">
         <div class="overlay"></div>
         <div class="container">
             <div class="row no-gutters slider-text js-fullheight align-items-center" data-scrollax-parent="true">
@@ -7,7 +7,7 @@
                     <div class="mt-5">
                         <h1 class="mb-4">Construire <br>Votre Maison !</h1>
                         <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
-                        <p><a href="#" class="btn btn-primary">Our Services</a> <a href="#" class="btn btn-white" data-toggle="modal" data-target="#exampleModalCenter">Nous Contacter</a></p>
+                        <p><a href="#" class="btn btn-primary">Nos Services</a> <a href="#" class="btn btn-white" data-toggle="modal" data-target="#exampleModalCenter">Nous Contacter</a></p>
                     </div>
                 </div>
             </div>
