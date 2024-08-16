b6b95622b013ed5e3ff6b129469b1d7664e5f27b
Bruno <bganontha@gmail.com>
2024-08-16 10:30:13 +0100

    finish with the admin footer page

--- resources/views/partials/admin/footer.blade.php
+++ resources/views/partials/admin/footer.blade.php
@@ -3,25 +3,25 @@
 					<nav class="pull-left">
 						<ul class="nav">
 							<li class="nav-item">
-								<a class="nav-link" href="http://www.themekita.com">
-									ThemeKita
+								<a class="nav-link" href="http://www.bellehouseniger.com">
+									Belle House
 								</a>
 							</li>
-							<li class="nav-item">
+							{{-- <li class="nav-item">
 								<a class="nav-link" href="#"> Help </a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link" href="#"> Licenses </a>
-							</li>
+							</li> --}}
 						</ul>
 					</nav>
 					<div class="copyright">
-						2024, made with <i class="fa fa-heart heart text-danger"></i> by
-						<a href="http://www.themekita.com">ThemeKita</a>
+						{{Date('Y')}}, concu <i class="fa fa-heart heart text-danger"></i> par
+						<a target="_blank" href="https://www.linkedin.com/in/bruno-ganontha-payang/">Ganontha Payang Bruno</a>
 					</div>
 					<div>
-						Distributed by
-						<a target="_blank" href="https://themewagon.com/">ThemeWagon</a>.
+						Tout Droit réservé
+						<a target="_blank" href="https://bellehouseniger.com/">Belle House</a>.
 					</div>
 				</div>
 			</footer>
