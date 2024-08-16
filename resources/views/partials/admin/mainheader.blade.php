1dda21899e08e72a171f08b7c1e2bf88ad6b34ed
Bruno <bganontha@gmail.com>
2024-08-16 10:30:35 +0100

    worked on the header of the page

--- resources/views/partials/admin/mainheader.blade.php
+++ resources/views/partials/admin/mainheader.blade.php
@@ -4,7 +4,7 @@
 					<div class="logo-header" data-background-color="dark">
 
 						<a href="../index.html" class="logo">
-							<img src="../assets/img/kaiadmin/logo_light.svg" alt="navbar brand" class="navbar-brand" height="20">
+							<img src="{{asset('images/logo.png')}}" alt="navbar brand" class="navbar-brand" height="20">
 						</a>
 						<div class="nav-toggle">
 							<button class="btn btn-toggle toggle-sidebar">
@@ -32,7 +32,7 @@
 										<i class="fa fa-search search-icon"></i>
 									</button>
 								</div>
-								<input type="text" placeholder="Search ..." class="form-control">
+								<input type="text" placeholder="Rechercher ..." class="form-control">
 							</div>
 						</nav>
 
@@ -177,89 +177,23 @@
 									</li>
 								</ul>
 							</li>
-							<li class="nav-item topbar-icon dropdown hidden-caret">
-								<a class="nav-link" data-bs-toggle="dropdown" href="#" aria-expanded="false">
-									<i class="fas fa-layer-group"></i>
-								</a>
-								<div class="dropdown-menu quick-actions animated fadeIn">
-									<div class="quick-actions-header">
-										<span class="title mb-1">Quick Actions</span>
-										<span class="subtitle op-7">Shortcuts</span>
-									</div>
-									<div class="quick-actions-scroll scrollbar-outer">
-										<div class="quick-actions-items">
-											<div class="row m-0">
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-danger rounded-circle">
-															<i class="far fa-calendar-alt"></i>
-														</div>
-														<span class="text">Calendar</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-warning rounded-circle">
-															<i class="fas fa-map"></i>
-														</div>
-														<span class="text">Maps</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-info rounded-circle">
-															<i class="fas fa-file-excel"></i>
-														</div>
-														<span class="text">Reports</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-success rounded-circle">
-															<i class="fas fa-envelope"></i>
-														</div>
-														<span class="text">Emails</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-primary rounded-circle">
-															<i class="fas fa-file-invoice-dollar"></i>
-														</div>
-														<span class="text">Invoice</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-secondary rounded-circle">
-															<i class="fas fa-credit-card"></i>
-														</div>
-														<span class="text">Payments</span>
-													</div>
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</li>
-							
 							<li class="nav-item topbar-user dropdown hidden-caret">
 								<a class="dropdown-toggle profile-pic" data-bs-toggle="dropdown" href="#" aria-expanded="false">
 									<div class="avatar-sm">
 										<img src="../assets/img/profile.jpg" alt="..." class="avatar-img rounded-circle">
 									</div>
 									<span class="profile-username">
-										<span class="op-7">Hi,</span> <span class="fw-bold">Hizrian</span>
+										<span class="op-7">Bonjour,</span> <span class="fw-bold">Admin</span>
 									</span>
 								</a>
 								<ul class="dropdown-menu dropdown-user animated fadeIn">
 									<div class="dropdown-user-scroll scrollbar-outer">
 										<li>
 											<div class="user-box">
-												<div class="avatar-lg"><img src="../assets/img/profile.jpg" alt="image profile" class="avatar-img rounded"></div>
+												<div class="avatar-lg"><img src="{{asset('images/person_1.jpg')}}" alt="image profile" class="avatar-img rounded"></div>
 												<div class="u-text">
-													<h4>Hizrian</h4>
-													<p class="text-muted">hello@example.com</p><a href="profile.html" class="btn btn-xs btn-secondary btn-sm">View Profile</a>
+													<h4>Admin</h4>
+													<p class="text-muted">contact@bellehouseniger.com</p><a href="profile.html" class="btn btn-xs btn-secondary btn-sm">View Profile</a>
 												</div>
 											</div>
 										</li>
