42da6c22d2225d67fb973a9bd576b21df97b5cc8
Bruno <bganontha@gmail.com>
2024-08-21 12:48:42 +0100

    added the provider to make data globally available

--- app/Providers/HomeServiceProvider.php
+++ app/Providers/HomeServiceProvider.php
@@ -19,8 +19,21 @@ class HomeServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        view()->composer(['partials.blog.blog_details'], function ($view) {
-            $view->with('blogs', \App\Models\Blog::all()->take(5));
+        view()->composer(['partials.blog.blog_details', 'partials.admin.mainheader'], function ($view) {
+            $view->with('blogs', \App\Models\Blog::all()->take(5))
+
+                ->with('messages', \App\Models\Contact::latest()->get()->map(
+                    function ($message) {
+                        $message->created_at_human = $message->created_at->locale('fr')->diffForHumans();
+                        return $message;
+                    }
+                ))
+                ->with('newMessages', \App\Models\Contact::where('read', false)->latest()->get()->map(
+                    function ($message) {
+                        $message->created_at_human = $message->created_at->locale('fr')->diffForHumans();
+                        return $message;
+                    }
+                ));
         });
     }
 }
