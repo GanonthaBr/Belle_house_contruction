b4a94604f3893f78382a82fd2854580d4ed0219c
Bruno <bganontha@gmail.com>
2024-09-19 09:43:33 +0100

    create a new blog

--- resources/views/partials/blog/blog_create.blade.php
+++ resources/views/partials/blog/blog_create.blade.php
@@ -1,41 +1,50 @@
-@extends('layouts.admin_main')
-@section('adminmain')
-<div class="container">
-    {{-- create a form to add new blog with title, image and content --}}
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">Ajouter un blog</h1>
-        {{-- display success message if blog is created --}}
-        @if (session()->has('blog-created'))
-        <div class="alert alert-success" role="alert">
-            {{ session()->get('blog-created') }}
+@extends('partials.admin.layout_admin')
+@section('admin')
+<div class="wrapper">
+    @include('partials.admin.sidebar')
+    <!-- End Sidebar -->
+    <div class="main-panel">
+        @include('partials.admin.mainheader')
+        <div class="container">
+            {{-- create a form to add new blog with title, image and content --}}
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">Ajouter un blog</h1>
+                {{-- display success message if blog is created --}}
+                @if (session()->has('blog-created'))
+                <div class="alert alert-success" role="alert">
+                    {{ session()->get('blog-created') }}
+                </div>
+                @endif
+                {{-- create form --}}
+                <form action="{{ route('blog.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Titre</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label for="author" class="form-label">Auteur:</label>
+                        <input type="text" class="form-control" id="author" name="author">
+                    </div>
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Image</label>
+                        <input type="file" class="form-control" id="image" name="image">
+                    </div>
+                    {{-- more images --}}
+                    <div class="mb-3">
+                        <label for="images" class="form-label">Plus d'images</label>
+                        <input type="file" class="form-control" id="images" name="images[]" multiple>
+                    </div>
+                    <div class="mb-3">
+                        <label for="content" class="form-label">Contenu</label>
+                        {{-- <textarea class="form-control" id="content" name="content" rows="3"></textarea> --}}
+                        <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Ajouter</button>
+                </form>
+            </div>
         </div>
-        @endif
-        {{-- create form --}}
-        <form action="{{ route('blog.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-                <label for="title" class="form-label">Titre</label>
-                <input type="text" class="form-control" id="title" name="title">
-            </div>
-            <div class="mb-3">
-                <label for="author" class="form-label">Auteur:</label>
-                <input type="text" class="form-control" id="author" name="author">
-            </div>
-            <div class="mb-3">
-                <label for="image" class="form-label">Image</label>
-                <input type="file" class="form-control" id="image" name="image">
-            </div>
-             {{-- more images --}}
-            <div class="mb-3">
-                <label for="images" class="form-label">Plus d'images</label>
-                <input type="file" class="form-control" id="images" name="images[]" multiple>
-            </div>
-            <div class="mb-3">
-                <label for="content" class="form-label">Contenu</label>
-                {{-- <textarea class="form-control" id="content" name="content" rows="3"></textarea> --}}
-                <textarea class="form-control" id="content" name="content" rows="3"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Ajouter</button>
-        </form>
+        @include('partials.admin.footer')
     </div>
+</div>
 @endsection
