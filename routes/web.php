92da131d3ab196e8b30924c7841c77c165f8fb01
Bruno <bganontha@gmail.com>
2024-10-02 15:37:04 +0100

    Rearrange the routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,63 @@ use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\RealisationController;
 use App\Http\Controllers\Auth\RegisterController;
 
+
+
+//admin
+
+//protected Routes
+Route::group(['middleware' => ['auth']], function () {
+    //the routes go here...
+    Route::get('/bhadmin', [HomeController::class, 'admin'])->name('admin');
+    Route::get('realisation/create', [RealisationController::class, 'create'])->name('realisation.create');
+    Route::post('/realisations', [RealisationController::class, 'store'])->name('realisation.store');
+    Route::get('/realisation/{id}/edit', [RealisationController::class, 'edit'])->name('realisation.edit');
+    Route::put('/realisation/{id}', [RealisationController::class, 'update'])->name('realisation.update');
+    Route::delete(
+        'realisation/{id}',
+        [RealisationController::class, 'destroy']
+    )->name('realisation.destroy');
+
+
+    Route::get('project/{id}/edit', [ProjectsController::class, 'edit'])->name('project.edit');
+    Route::put('project/{id}', [ProjectsController::class, 'update'])->name('project.update');
+    Route::delete('/project/delete/{id}', [ProjectsController::class, 'destroy'])->name('project.destroy');
+    Route::post('/projects', [ProjectsController::class, 'store'])->name('project.store');
+    Route::get('/project/create', [ProjectsController::class, 'create'])->name('project.create');
+
+
+
+    Route::get(
+        '/blog/{id}/edit',
+        [BlogController::class, 'edit']
+    )->name('blog.edit');
+    Route::put('/blog/{id}', [BlogController::class, 'update'])->name('blog.update');
+    Route::delete('/blog/{id}', [BlogController::class, 'destroy'])->name('blog.destroy');
+    Route::get('/blog/create', [BlogController::class, 'create'])->name('blog.create');
+    Route::post('/blogs', [BlogController::class, 'store'])->name('blog.store');
+
+
+
+    Route::get(
+        'service/create',
+        [ServicesController::class, 'create']
+    )->name('service.create');
+    Route::post('/services', [ServicesController::class, 'store'])->name('service.store');
+    Route::get('service/{id}/edit', [ServicesController::class, 'edit'])->name('service.edit');
+    Route::put('service/{id}', [ServicesController::class, 'update'])->name('service.update');
+    Route::delete('service/delete/{id}', [ServicesController::class, 'destroy'])->name('service.destroy');
+
+
+    Route::get(
+        'partner/create',
+        [PartnerController::class, 'create']
+    )->name('partner.create');
+    Route::post('/partners', [PartnerController::class, 'store'])->name('partner.store');
+    Route::get('/partner/{id}/edit', [PartnerController::class, 'edit'])->name('partner.edit');
+    Route::put('/partner/{id}', [PartnerController::class, 'update'])->name('partner.update');
+    Route::delete('partner/{id}', [PartnerController::class, 'destroy'])->name('partner.destroy');
+});
+
 //Home 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -33,10 +90,6 @@ Route::post('/modal', [ModalController::class, 'store'])->name('modal.store');
 
 
 
-//services
-Route::get('/services', [ServicesController::class, 'index'])->name('services');
-Route::get('service/{id}', [ServicesController::class, 'show'])->name('service.show');
-Route::get('allservices', [ServicesController::class, 'allservices'])->name('allservices');
 
 //blogs
 Route::get('/blogs', [BlogController::class, 'index'])->name('blogs');
@@ -71,54 +124,11 @@ Route::post('/register-from-blog', [RegisterController::class, 'registerBlog'])-
 Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::delete('/comment/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
 
-//admin
-
-//protected Routes
-Route::group(['middleware' => ['auth']], function () {
-    //the routes go here...
-    Route::get('/bhadmin', [HomeController::class, 'admin'])->name('admin');
-    Route::post('/realisations', [RealisationController::class, 'store'])->name('realisation.store');
-    Route::get('/realisation/{id}/edit', [RealisationController::class, 'edit'])->name('realisation.edit');
-    Route::put('/realisation/{id}', [RealisationController::class, 'update'])->name('realisation.update');
-    Route::delete(
-        'realisation/{id}',
-        [RealisationController::class, 'destroy']
-    )->name('realisation.destroy');
-    Route::get('realisation/create', [RealisationController::class, 'create'])->name('realisation.create');
-
-
-    Route::get('project/{id}/edit', [ProjectsController::class, 'edit'])->name('project.edit');
-    Route::put('project/{id}', [ProjectsController::class, 'update'])->name('project.update');
-    Route::delete('/project/delete/{id}', [ProjectsController::class, 'destroy'])->name('project.destroy');
-    Route::post('/projects', [ProjectsController::class, 'store'])->name('project.store');
-    Route::get('/project/create', [ProjectsController::class, 'create'])->name('project.create');
 
 
 
-    Route::get(
-        '/blog/{id}/edit',
-        [BlogController::class, 'edit']
-    )->name('blog.edit');
-    Route::put('/blog/{id}', [BlogController::class, 'update'])->name('blog.update');
-    Route::delete('/blog/{id}', [BlogController::class, 'destroy'])->name('blog.destroy');
-    Route::get('/blog/create', [BlogController::class, 'create'])->name('blog.create');
-    Route::post('/blogs', [BlogController::class, 'store'])->name('blog.store');
 
-
-
-    Route::get('/service/create', [ServicesController::class, 'create'])->name('service.create');
-    Route::post('/services', [ServicesController::class, 'store'])->name('service.store');
-    Route::get('service/{id}/edit', [ServicesController::class, 'edit'])->name('service.edit');
-    Route::put('service/{id}', [ServicesController::class, 'update'])->name('service.update');
-    Route::delete('service/delete/{id}', [ServicesController::class, 'destroy'])->name('service.destroy');
-
-
-    Route::get(
-        'partner/create',
-        [PartnerController::class, 'create']
-    )->name('partner.create');
-    Route::post('/partners', [PartnerController::class, 'store'])->name('partner.store');
-    Route::get('/partner/{id}/edit', [PartnerController::class, 'edit'])->name('partner.edit');
-    Route::put('/partner/{id}', [PartnerController::class, 'update'])->name('partner.update');
-    Route::delete('partner/{id}', [PartnerController::class, 'destroy'])->name('partner.destroy');
-});
+//services
+Route::get('/services', [ServicesController::class, 'index'])->name('services');
+Route::get('service/{id}', [ServicesController::class, 'show'])->name('service.show');
+Route::get('allservices', [ServicesController::class, 'allservices'])->name('allservices');
