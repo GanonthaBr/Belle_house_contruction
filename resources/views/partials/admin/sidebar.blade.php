9a68d20432d49554ebe017818f3726c63e28b046
Bruno <bganontha@gmail.com>
2024-10-02 13:11:21 +0100

    Icon colors on the sidebr

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -26,23 +26,23 @@
 			<ul class="nav nav-secondary">
 				<li class="nav-item">
 					<a data-bs-toggle="collapse" href="{{route('admin')}}" class="collapsed" aria-expanded="false">
-						<i class="fas fa-home"></i>
+					<i class="fas fa-home icon"></i> 
 						<p>Menu</p>
-						<span class="caret"></span>
+						<span class="caret icon"></span>
 					</a>
 				</li>
 				<li class="nav-section">
 					<span class="sidebar-mini-icon">
-						<i class="fa fa-ellipsis-h"></i>
+						<i class="fa fa-ellipsis-h icon"></i>
 					</span>
 					<h4 class="text-section">Composants</h4>
 				</li>
 				<li class="nav-item">
 					{{-- data-bs-target="#forms-nav" data-bs-toggle="collapse"  --}}
 					<a data-bs-toggle="collapse" data-bs-target="#sidebarLayouts"  href="#">
-						<i class="fas fa-th-list"></i>
+						<i class="fas fa-th-list icon"></i>
 						<p>Les Realisations</p>
-						<span class="caret"></span>
+						<span class="caret icon"></span>
 					</a>
 					<div class="collapse" id="sidebarLayouts" >
 						<ul class="nav nav-collapse">
@@ -61,9 +61,9 @@
 				</li>
 				<li class="nav-item">
 					<a data-bs-toggle="collapse" data-bs-target="#forms" href="#">
-						<i class="fas fa-pen-square"></i>
+						<i class="fas fa-pen-square icon"></i>
 						<p>Les Projets</p>
-						<span class="caret"></span>
+						<span class="caret icon"></span>
 					</a>
 					<div class="collapse" id="forms">
 						<ul class="nav nav-collapse">
@@ -82,9 +82,9 @@
 				</li>
 				<li class="nav-item">
 					<a data-bs-toggle="collapse" data-bs-target="#tables" href="#">
-						<i class="fas fa-table"></i>
+						<i class="fas fa-table icon"></i>
 						<p>Blog Post</p>
-						<span class="caret"></span>
+						<span class="caret icon"></span>
 					</a>
 					<div class="collapse" id="tables">
 						<ul class="nav nav-collapse">
@@ -103,9 +103,9 @@
 				</li>
 				<li class="nav-item">
 					<a data-bs-toggle="collapse" data-bs-target="#tables01" href="#">
-						<i class="fas fa-table"></i>
+						<i class="fas fa-table icon"></i>
 						<p>Nos Services</p>
-						<span class="caret"></span>
+						<span class="caret icon"></span>
 					</a>
 					<div class="collapse" id="tables01">
 						<ul class="nav nav-collapse">
@@ -124,9 +124,9 @@
 				</li>
 				<li class="nav-item">
 					<a data-bs-toggle="collapse" data-bs-target="#maps" href="#">
-						<i class="fas fa-map-marker-alt"></i>
+						<i class="fas fa-map-marker-alt icon"></i>
 						<p>Partenaires</p>
-						<span class="caret"></span>
+						<span class="caret icon"></span>
 					</a>
 					<div class="collapse" id="maps">
 						<ul class="nav nav-collapse">
@@ -145,7 +145,10 @@
 
 				</li>
 				<li class="nav-item">
-					<a href="{{route('messages')}}">Messages >></a>
+					<a href="{{route('messages')}}">Messages</a>
+				</li>
+				<li class="nav-item">
+					<a href="{{route('messages')}}">Statistiques</a>
 				</li>
 			</ul>
 		</div>
